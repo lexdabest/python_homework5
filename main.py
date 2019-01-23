0fc2bf5ceed1256220ee6f78e29b042a7e26b346
Alex ofobrukweta <alexgraphicspro@yahoo.com>
2019-01-23 11:36:17 +0100

    Python Assignment 5

---
+++ main.py
@@ -0,0 +1,22 @@
+flag = False
+for i in range(1,101): 
+    for j in range(2,i):
+        if i%j == 0:
+            flag = True
+            break
+        else:
+            flag = False
+    if flag == False and i!=1:
+        print("prime") 
+    else:
+        fizz = i%3
+        buzz = i%5
+        if fizz == 0 and buzz == 0:
+            print("FizzBuzz")
+        elif fizz == 0:
+            print("Fizz")
+        elif buzz == 0:
+            print("Buzz")
+        else:
+            print(i)
+   
